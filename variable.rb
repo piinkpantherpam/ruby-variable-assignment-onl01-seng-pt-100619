62695fef82749778b45d5f67974f29714a2fc40b
Pam Cairo <pcairo_90@outlook.com>
2019-10-12 21:45:16 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,4 +1,4 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
 current_president = "Donald Trump"
-puts "In 2017, the president ."
+puts "In 2017, the president was #{current_president}."
