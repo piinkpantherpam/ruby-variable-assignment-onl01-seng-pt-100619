1d21559de34367f3501cdf3b05ba8d26bc84040c
Pam Cairo <pcairo_90@outlook.com>
2019-10-12 21:48:51 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -2,3 +2,6 @@
  # give it the value of the string "Hello World"
 current_president = "Donald Trump"
 puts "In 2017, the president was #{current_president}."
+
+greeting = "Hello World"
+puts "Colton Underwood and OTOWN collaborated on a song in 2019 titled, #{greeting}."
