e8b7e319ba12ec02ced8f28fc7247b155fd1e7f8
Pam Cairo <pcairo_90@outlook.com>
2019-10-12 21:49:16 +0000

    Done.

--- variable.rb
+++ variable.rb
@@ -1,2 +1,7 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
+current_president = "Donald Trump"
+puts "In 2017, the president was #{current_president}."
+
+greeting = "Hello World"
+puts "Colton Underwood and OTOWN collaborated on a song in 2019 titled, #{greeting}."
